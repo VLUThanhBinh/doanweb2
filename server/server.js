512f4eae9d853ca93555c1ea67474baf94e93464
NguyenTrongAnh <anhnguyen20040312@gmail.com>
2025-04-16 20:41:04 +0700

    1

--- server/server.js
+++ server/server.js
@@ -1,8 +1,15 @@
-const express = require("express");
+import { connectDB } from "./config/db.js";
+import dotenv from "dotenv";
+import express from "express";
+
+dotenv.config();
+
 const app = express();
 const port = 8000;
 
 app.get("/products", (req, res) => res.send("Hello World!"));
-app.listen(port, () =>
-  console.log(`Server is running at http://localhost:${port}`)
-);
+
+app.listen(port, () => {
+  connectDB();
+  console.log(`Server is running at http://localhost:${port}`);
+});
